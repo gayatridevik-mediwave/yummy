3183ac3f98a696d364ab4bbad7f6f2a0b953d832
gayatridevik-mediwave <gayatridevi_k@mindwaveventures.com>
2022-02-09 11:24:15 +0530

    new edit

--- route.js
+++ route.js
@@ -33,11 +33,11 @@ router.get("/:id", (req, res) => {
          FROM
          yummy_recipe yr
         INNER JOIN recipe_image ri 
-        ON yr.recipe_id=ri.recipe_id
+        ON yr.recipe_id = ri.recipe_id
         INNER JOIN yummy_recipe_desc yrd
-        ON yr.recipe_id=yrd.recipe_id
+        ON yr.recipe_id = yrd.recipe_id
         WHERE 
-        yr.recipe_id=?;`
+        yr.recipe_id =?; `
         )
         .get(req.params.id);
     if (!recipes) {
@@ -67,9 +67,9 @@ router.post("/", (req, res) => {
     }
 
     const addRecipe = db.prepare(
-        `Insert INTO yummy_recipe 
-        (recipe_name,food_type)
-         VALUES (@recipe_name,@food_type);`
+        `Insert INTO yummy_recipe
+        (recipe_name, food_type)
+    VALUES(@recipe_name, @food_type); `
     ).run({
         recipe_name: payload.recipe_name,
         food_type: payload.food_type
@@ -78,8 +78,8 @@ router.post("/", (req, res) => {
 
     const recipeImage = db.prepare(
         `Insert INTO recipe_image
-        (recipe_id,img_url,img_alt)
-        VALUES (@newRecipeId,@img_url,@img_alt);`
+    (recipe_id, img_url, img_alt)
+VALUES(@newRecipeId, @img_url, @img_alt); `
     ).run({
         newRecipeId: newRecipeId,
         img_url: payload.img_url,
@@ -87,8 +87,8 @@ router.post("/", (req, res) => {
     });
     const recipeDesc = db.prepare(
         `Insert INTO yummy_recipe_desc
-    (recipe_id,ing_req,steps_to_prepare,description) 
-    VALUES (@newRecipeId,@ing_req,@steps_to_prepare,@description);`
+    (recipe_id, ing_req, steps_to_prepare, description)
+VALUES(@newRecipeId, @ing_req, @steps_to_prepare, @description); `
     ).run({
         newRecipeId: newRecipeId,
         ing_req: payload.ing_req,
@@ -110,102 +110,184 @@ router.post("/", (req, res) => {
 
 // //update a recipe
 
-router.put("/:id", (req, res) => {
-    const payload = req.body;
-    if (!payload.recipe_name ||
-        !payload.food_type ||
-        !payload.img_url ||
-        !payload.img_alt ||
-        !payload.ing_req ||
-        !payload.steps_to_prepare ||
-        !payload.description) {
-        return res.status(404).send({
-            message: "Enter all details about recipe",
-        });
-    }
+// router.put("/:id", (req, res) => {
+//     const payload = req.body;
+//     if (!payload.recipe_name ||
+//         !payload.food_type ||
+//         !payload.img_url ||
+//         !payload.img_alt ||
+//         !payload.ing_req ||
+//         !payload.steps_to_prepare ||
+//         !payload.description) {
+//         return res.status(201).send({
+//             message: "Enter all details about recipe",
+//         });
+//     }
 
-    const updateOneRecipe = db.prepare(
-        `SELECT yr.recipe_id ID,
-            yr.recipe_name Recipe,
-            yr.food_type,
-            ri.img_url Image,
-            ri.img_alt Imgalt,
-            rid.ing_reg,
-            rid steps_to_prepare,
-            rid description) 
-            FROM 
-            yummy_recipe yr
-            INNER JOIN recipe_image ri
-            ON yr.recipe_id = ri recipe_id
-            INNER JOIN yummy_recipe_desc yrd
-            ON yr.recipe_id = yrd.recipe_id
-            WHERE yr.recipe_id =?;`
-    )
-        .get(req.params.id);
-    if (!updateOneRecipe) {
-        return res.status(404).send({
-            message: `[req.params.id] recipe not found`,
-        });
-    }
-    db.prepare(
-        `UPDATE yummy_recipe
-        SET
-        recipe_name= @recipe_name,
-        food_type = @ food_type,
-        WHERE 
-        recipe_id = @ recipe_id;`
-    )
-        .run({
-            recipe_id: req.params.id,
-            recipe_name: payload.recipe_name,
-            food_type: payload.food_type,
-        });
+//     const updateOneRecipe = db.prepare(
+//         `SELECT
+// yr.recipe_id ID,
+//     yr.recipe_name RECIPE,
+//         yr.food_type FoodType,
+//             ri.img_url ImageURL,
+//                 ri.img_alt ImageInfo,
+//                     yrd.ing_req IngridentReq,
+//                         yrd.steps_to_prepare PrepSteps,
+//                             yrd.description RecipeDesc
+// FROM
+//          yummy_recipe yr
+//         INNER JOIN recipe_image ri 
+//         ON yr.recipe_id = ri.recipe_id
+//         INNER JOIN yummy_recipe_desc yrd
+//         ON yr.recipe_id = yrd.recipe_id
+// WHERE
+// yr.recipe_id =?; `
+//     )
+//         .get(req.params.id);
+//     if (!updateOneRecipe) {
+//         return res.status(404).json({
+//             message: `${req.params.id} recipe not found`,
+//         });
+//     }
+//     db.prepare(
+//         `UPDATE yummy_recipe
+// SET
+// recipe_name = @recipe_name,
+// food_type = @food_type,
+// WHERE
+// recipe_id = @recipe_id; `
+//     ).run({
+//         recipe_id: req.params.id,
+//         recipe_name: payload.recipe_name,
+//         food_type: payload.food_type,
+//     });
 
-    db.prepare(
-        `UPDATE recipe_image
-            SET
-            recipe_id = @ recipe_id
-            img_url = @ img_url
-            img_alt = @ img_alt
-            WHERE 
-            recipe_id: @recipe_id`
-    ).run({
-        recipe_id: req.params.recipe_id,
-        img_url: payload.img_url,
-        img_alt: payload.img_alt,
-    });
+//     db.prepare(
+//         `UPDATE recipe_image
+// SET
+// recipe_id = @recipe_id
+// img_url = @img_url
+// img_alt = @img_alt
+// WHERE
+// recipe_id: @recipe_id`
+//     ).run({
+//         recipe_id: req.params.recipe_id,
+//         img_url: payload.img_url,
+//         img_alt: payload.img_alt,
+//     });
 
-    db.prepare(
-        `UPDATE  yummy_recipe_desc
-        SET 
-        recipe_id = @recipe_id
-        ing_req = @ ing_req
-        steps_to_prepare = @ steps_to_prepare
-        WHERE 
-        recipe_id: @recipe_id`
-    ).run({
-        recipe_id: req.params.recipe_id,
-        ing_req: payload.ing_req,
-        steps_to_prepare: payload.steps_to_prepare,
-        desc: payload.desc,
-    });
-    console.log(JSON.stringify(payload));
-    return res.status(201).send({
-        recipe_id: req.params.recipe_id,
-        recipe_name: req.params.recipe_name,
-        food_type: req.params.food_type,
-        img_url: req.params.img_url,
-        img_alt: req.params.img_alt,
-        ing_req: req.params.ing_req,
-        steps_to_prepare: req.params.steps_to_prepare,
-        desc: req.params.desc,
-    });
-});
+//     db.prepare(
+//         `UPDATE  yummy_recipe_desc
+// SET
+// recipe_id = @recipe_id
+// ing_req = @ing_req
+// steps_to_prepare = @steps_to_prepare
+// WHERE
+// recipe_id: @recipe_id`
+//     ).run({
+//         recipe_id: req.params.recipe_id,
+//         ing_req: payload.ing_req,
+//         steps_to_prepare: payload.steps_to_prepare,
+//         desc: payload.desc,
+//     });
+//     console.log(JSON.stringify(payload));
+//     return res.status(201).send({
+//         recipe_id: req.params.recipe_id,
+//         recipe_name: req.params.recipe_name,
+//         food_type: req.params.food_type,
+//         img_url: req.params.img_url,
+//         img_alt: req.params.img_alt,
+//         ing_req: req.params.ing_req,
+//         steps_to_prepare: req.params.steps_to_prepare,
+//         desc: req.params.desc,
+//     });
+// });
 
 //delete a recipe
 router.delete("/:id", (req, res) => {
     const deleteOneRecipe = db.
         prepare(
+            `SELECT
+                yr.recipe_id ID,
+                yr.recipe_name RECIPE,
+                yr.food_type FoodType,
+                ri.img_url ImageURL,
+                ri.img_alt ImageInfo,
+                yrd.ing_req IngridentReq,
+                yrd.steps_to_prepare PrepSteps,
+                yrd.description RecipeDesc
+FROM
+         yummy_recipe yr
+        INNER JOIN recipe_image ri 
+        ON yr.recipe_id = ri.recipe_id
+        INNER JOIN yummy_recipe_desc yrd
+        ON yr.recipe_id = yrd.recipe_id
+WHERE
+yr.recipe_id =?; `
+        )
+        .get(req.params.id);
+    if (!deleteOneRecipe) {
+        return res.status(404).json({
+            message: `${req.params.id} recipe not found`,
+        });
+    }
+    db.prepare(
+        `DELETE FROM
+recipe_image
+WHERE
+recipe_id =@recipe_id; `
+    ).run({ recipe_id: req.params.id });
+    db.prepare(
+        `DELETE FROM yummy_recipe_desc
+        WHERE recipe_id =@recipe_id; `
+    ).run({ recipe_id: req.params.id });
+    db.prepare(
+        `DELETE FROM yummy_recipe
+        WHERE recipe_id =@recipe_id; `
+    ).run({ recipe_id: req.params.id });
+
+    return res.status(201).send({ deleteOneRecipe });
+});
+
+module.exports = router;
+
+
+
+// PRAGMA table_info(yummy_recipe);
+// 0|recipe_id|INTEGER|1||1
+// 1|recipe_name|VARCHAR(256)|0||0
+// 2|food_type|VARCHAR(256)|0||0
+
+// sqlite> PRAGMA table_info(recipe_image);
+// 0|img_id|INTEGER|1||1
+// 1|recipe_id|INTEGER|0||0
+// 2|img_url|VARCHAR(256)|0||0
+// 3|img_alt|VARCHAR(256)|0||0
+
+// sqlite> PRAGMA table_info(yummy_recipe_desc);
+// 0|des_id|INTEGER|1||1
+// 1|recipe_id|INTEGER|0||0
+// 2|ing_req|TEXT|0||0
+// 3|steps_to_prepare|TEXT|0||0
+// 4|description|TEXT|0||0
+
+
+//update a recipe
+router.put("/:id", (req, res) => {
+    const payload = req.body;
+    if (
+        !payload.recipe_name ||
+        !payload.img_url ||
+        !payload.ing_req ||
+        !payload.steps_to_prepare ||
+        !payload.description
+    ) {
+        return res.status(400).send({
+            message: "Enter all details about recipe",
+        });
+    }
+    const updateOneRecipe = db
+        .prepare(
             `SELECT
         yr.recipe_id ID,
         yr.recipe_name RECIPE,
@@ -218,56 +300,67 @@ router.delete("/:id", (req, res) => {
          FROM
          yummy_recipe yr
         INNER JOIN recipe_image ri 
-        ON yr.recipe_id=ri.recipe_id
+        ON yr.recipe_id = ri.recipe_id
         INNER JOIN yummy_recipe_desc yrd
-        ON yr.recipe_id=yrd.recipe_id
+        ON yr.recipe_id = yrd.recipe_id
         WHERE 
-        yr.recipe_id=?;`
+        yr.recipe_id =?; `
         )
         .get(req.params.id);
-    if (!deleteOneRecipe) {
+    if (!updateOneRecipe) {
         return res.status(404).send({
             message: `${req.params.id} recipe not found`,
         });
     }
     db.prepare(
-        `DELETE FROM 
-        recipe_image
-        WHERE
-     recipe_id =@ recipe_id;`
-    ).run({ recipe_id: req.params.id });
+        `UPDATE 
+        yummy_recipe
+      SET 
+      recipe_name=@recipe_name,
+      food_type=@food_type
+      WHERE 
+      recipe_id=@recipe_id;`
+    ).run({
+        recipe_id: req.params.id,
+        recipe_name: payload.recipe_name,
+        food_type: payload.food_type,
+    });
     db.prepare(
-        `DELETE FROM yummy_recipe_desc
-        WHERE recipe_id =@recipe_id;`
-    ).run({ recipe_id: req.params.id });
+        `UPDATE recipe_image 
+      SET 
+          recipe_id=@recipe_id,
+          img_url=@img_url,
+          img_alt=@img_alt
+      WHERE 
+      recipe_id=@recipe_id;`
+    ).run({
+        recipe_id: req.params.recipe_id,
+        img_url: payload.img_url,
+        img_alt: payload.img_alt,
+    });
     db.prepare(
-        `DELETE FROM yummy_recipe
-        WHERE recipe_id =@recipe_id;`
-    ).run({ recipe_id: req.params.id });
-
-    return res.status(201).send({ deleteOneRecipe });
+        `UPDATE yummy_recipe_desc 
+        SET 
+        recipe_id=@recipe_id,
+        ing_req=@ing_req,
+        steps_to_prepare=@steps_to_prepare,
+        description=@description
+        WHERE 
+        recipe_id=@recipe_id;`
+    ).run({
+        recipe_id: req.params.recipe_id,
+        ing_req: payload.ing_req,
+        steps_to_prepare: payload.steps_to_prepare,
+        description: payload.description,
+    });
+    console.log(JSON.stringify(payload));
+    return res.status(201).send({
+        recipe_id: req.params.recipe_id,
+        recipe_name: payload.recipe_name,
+        img_url: payload.img_url,
+        img_alt: payload.img_alt,
+        ing_req: payload.ing_req,
+        steps_to_prepare: payload.steps_to_prepare,
+        description: payload.description,
+    });
 });
-
-module.exports = router;
-
-
-
-    // PRAGMA table_info(yummy_recipe);
-    // 0|recipe_id|INTEGER|1||1
-    // 1|recipe_name|VARCHAR(256)|0||0
-    // 2|food_type|VARCHAR(256)|0||0
-
-    // sqlite> PRAGMA table_info(recipe_image);
-    // 0|img_id|INTEGER|1||1
-    // 1|recipe_id|INTEGER|0||0
-    // 2|img_url|VARCHAR(256)|0||0
-    // 3|img_alt|VARCHAR(256)|0||0
-
-    // sqlite> PRAGMA table_info(yummy_recipe_desc);
-    // 0|des_id|INTEGER|1||1
-    // 1|recipe_id|INTEGER|0||0
-    // 2|ing_req|TEXT|0||0
-    // 3|steps_to_prepare|TEXT|0||0
-    // 4|description|TEXT|0||0
-
-
